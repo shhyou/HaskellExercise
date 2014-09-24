349bed745022eb6f0199a3c5c83239c2d690ec79
suhorng <suhorng@gmail.com>
2014-09-24 10:39:02 +0800

    add object-like syntax for simple functions

---
+++ ObjDot.hs
@@ -0,0 +1,16 @@
+module ObjDot where
+
+the :: a -> (a -> b) -> b
+the = flip ($)
+
+test :: [(Int,Int,Int)]
+test = the([1..10]).concatMap $ \a ->
+         the([1..10]).concatMap $ \b ->
+          the([1..10]).concatMap $ \c ->
+            if a^2+b^2 == c^2
+              then [(a,b,c)]
+              else []
+
+test2 :: [String]
+test2 = the(test).map $ \(a,b,c) ->
+          show a ++ "^2 + " ++ show b ++ "^2 = " ++ show c
