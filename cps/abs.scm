7b3e0c8abb1d4a151c11d4072d4a9529da422129
suhorng <suhorng@gmail.com>
2015-08-29 16:46:18 +0800

    Implement CPS transform algorithm in Abstracting Control S2

--- cps/abs.scm
+++ cps/abs.scm
@@ -1,21 +1,51 @@
 (define-module abs
-  (export cps)
+  (use util.match :only (match))
+  (export cps))
+
+(select-module abs)
+
+(define (cps expr)
+  (define fresh
+    (let ([cnt 1])
+      (lambda (s)
+        (let ([old-cnt cnt])
+          (set! cnt (+ cnt 1))
+          ($ string->symbol $ string-append s $ number->string old-cnt)))))
   (define (cpsk expr k)
     (match expr
       [(? (lambda (v) (or (symbol? v) (number? v))) v)
        (k v)]
       [('lambda (x) e)
-       #f]
+       (let ([k1 (fresh "k")])
+         `(lambda (,x ,k1) ,(cpsk e (lambda (v) `(,k1 ,v)))))]
       [('callcc f)
-       #f]
+       (cpsk f (lambda (f^)
+                 (let* ([k^ (fresh "k")]
+                        [k1 (fresh "k")]
+                        [tmp (fresh "%")]
+                        [v (fresh "%")])
+                   `(let ([,k^ (lambda (,tmp) ,(k tmp))])
+                      (,f^ (lambda (,v ,k1) (,k^ ,v)) ,k^)))))]
       [('reset e)
-       #f]
+       (let ([tmp (fresh "%")])
+         `((lambda (,tmp) ,(k tmp)) ,(cpsk e (lambda (x) x))))]
       [('shift f)
-       #f]
+       (let ([k1 (fresh "k")]
+             [v (fresh "v")]
+             [x (fresh "x")])
+         (cpsk f (lambda (f^)
+                   `(,f^ (lambda (,v ,k1) (,k1 ,(k v))) (lambda (,x) ,x)))))]
       [('if e1 e2 e3)
-       #f]
+       (let* ([k1 (fresh "k")]
+              [v (fresh "%")])
+         `(let ([,k1 (lambda (,v) ,(k v))])
+            ,(cpsk e1 (lambda (v)
+                        `(if ,v
+                             ,(cpsk e2 (lambda (v1) `(,k1 ,v1)))
+                             ,(cpsk e3 (lambda (v1) `(,k1 ,v1))))))))]
       [(e1 e2)
-       #f]))
-  
-  (define (cps expr)
-    (cpsk expr (lambda (v) v))))
+       (let ([tmp (fresh "%")])
+         (cpsk e1 (lambda (f)
+                    (cpsk e2 (lambda (v)
+                               `(,f ,v (lambda (,tmp) ,(k tmp))))))))]))
+  (cpsk expr (lambda (v) v)))
