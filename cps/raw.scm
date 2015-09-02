1be1c65af46b6716f61d5c29a79519b373170b16
suhorng <suhorng@gmail.com>
2015-09-02 19:35:20 +0800

    currying raw-transformation and add reset,shift,if cases

--- cps/raw.scm
+++ cps/raw.scm
@@ -13,19 +13,44 @@
           ($ string->symbol $ string-append s $ number->string old-cnt)))))
   (define (cpsk expr)
     (match expr
-      [(? symbol? x)
+      [(? (lambda (v) (or (symbol? v) (number? v))) v)
        (let ([k (fresh "k")])
-         `(lambda (,k) (,k ,x)))]
+         `(lambda (,k) (,k ,v)))]
       [('lambda (x) e)
        (let* ([k0 (fresh "k")]
               [k1 (fresh "k")])
-         `(lambda (k0) (,k0 (lambda (,x ,k1) (,(cpsk e) ,k1)))))]
+         `(lambda (k0) (,k0 (lambda (,x)
+                              ,(cpsk e)))))]
       [('callcc f)
        (let* ([k (fresh "k")]
               [k1 (fresh "k")]
               [v (fresh "v")])
          `(lambda (,k)
-            (,f (lambda (,v ,k1) (,k ,v)) k)))]
+            (,f (lambda (,v)
+                  (lambda (,k1)
+                    (,k ,v)))
+                k)))]
+      [('reset e)
+       (let ([k (fresh "k")])
+         `(lambda (,k)
+            (,k (,(cpsk e) (lambda (%0) %0)))))]
+      [('shift f)
+       (let ([k (fresh "k")]
+             [k1 (fresh "k")]
+             [v (fresh "v")])
+         `(lambda (,k)
+            ((,(cpsk f) (lambda (,v)
+                          (lambda (,k1)
+                            (,k1 (,k ,v)))))
+             (lambda (%0) %0))))]
+      [('if e1 e2 e3)
+       (let* ([k (fresh "k")]
+              [v (fresh "v")])
+         `(lambda (,k)
+            (,(cpsk e1) (lambda (,v)
+                          (if ,v
+                              (,(cpsk e2) ,k)
+                              (,(cpsk e3) ,k))))))]
       [(e1 e2)
        (let ([f (fresh "%")]
              [v (fresh "%")]
@@ -35,5 +60,5 @@
              (lambda (,f)
                ,(cpsk e2)
                (lambda (,v)
-                 (,f ,v ,k))))))]))
+                 ((,f ,v) ,k))))))]))
   (cpsk expr))
